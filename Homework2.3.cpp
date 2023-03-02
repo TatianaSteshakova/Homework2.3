7e66cce473349b5dcdb4fb161d5e8dc7380bafa1
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-02 10:01:39 +0300

    Add files via upload

---
+++ Homework2.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+
+    long int login = 0;
+    long int pass = 0;
+
+    cout << "Введите логин и пароль: " << endl;
+    cin >> login >> pass;
+
+    if ((login == 10000 && pass == 20000) || (login == 55555 && pass == 66666) || (login == 12345 && pass == 67890))
+        cout << "Добро пожаловать!";
+    else
+        cout << "Неверный логин или пароль!";
+}
